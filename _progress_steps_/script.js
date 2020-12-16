5ed5b2df2ab647b01d6b6db2aa86b1518b306765
Júlia Mendes <juliadepaivams@gmail.com>
2020-12-16 19:27:01 -0300

    Day three rotation navigation

--- _progress_steps_/script.js
+++ _progress_steps_/script.js
@@ -37,7 +37,7 @@ function update () {
 
     const actives = document.querySelectorAll('.active')
 
-    progress.style.width = (actives.length -1) / (circles.length -1)* 100 + '%'
+    progress.style.width = (actives.length - 1) / (circles.length -1)* 100 + '%'
 
     if(currentActive == 1) {
         prev.disabled = true
